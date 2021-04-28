6df3b8680eb5175f9e507f52a8828267f91a9c9a
zhujilong <zhujilong2007@sina.com>
2021-04-28 16:37:41 +0800

    项目列表接口完成，但是listView还是有问题

--- common/http.api.js
+++ common/http.api.js
@@ -6,7 +6,12 @@ let normalArticle = 'article/list/'
 
 let getSearchHotKey = 'hotkey/json'
 
+/// 应该是post请求都有异常,需要跟踪调试
 let queryKey = 'article/query/'
+
+let getProjectClassify = 'project/tree/json';
+
+let getProjectClassifyList = 'project/list/';
 
 // 此处第二个参数vm，就是我们在页面使用的this，你可以通过vm获取vuex等操作，更多内容详见uView对拦截器的介绍部分：
 // https://uviewui.com/js/http.html#%E4%BD%95%E8%B0%93%E8%AF%B7%E6%B1%82%E6%8B%A6%E6%88%AA%EF%BC%9F
@@ -27,8 +32,14 @@ const install = (Vue, vm) => {
 	// 关键词搜索
 	let queryKeyword = (params = {}, page) => vm.$u.post(queryKey  + '0' + '/json', params);
 	
+	// 项目分类
+	let projectTopic = (params = {}) => vm.$u.get(getProjectClassify, params);
+	
+	// 项目列表
+	let projectList = (params = {}, page) => vm.$u.get(getProjectClassifyList+ page.toString() + "/json", params);
+	
 	// 将各个定义的接口名称，统一放进对象挂载到vm.$u.api(因为vm就是this，也即this.$u.api)下
-	vm.$u.api = {banner, top, normal, hotKey, queryKeyword};
+	vm.$u.api = {banner, top, normal, hotKey, queryKeyword, projectTopic, projectList};
 }
 
 export default {
--- pages/infomation/index.vue
+++ pages/infomation/index.vue
@@ -1,50 +1,92 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/uview/common/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view>
+		<view>
+			<u-tabs-swiper ref="uTabs" :list="topics" @change="tabsChange" :is-scroll="true" swiperWidth="750"></u-tabs-swiper>
 		</view>
+		<swiper class="swiper-box" :current="swiperCurrent" @transition="transition" @animationfinish="animationfinish">
+			<swiper-item class="swiper-item" v-for="(item, index) in topics" :key="index">
+				<scroll-view scroll-y style="height: 100%;width: 100%;" @scrolltolower="onreachBottom">
+					<view v-for="(model,idx) in list" :key="idx">
+						<u-cell-item :title="model.title" :label="model.author" :index="index" @click=""></u-cell-item>
+					</view>
+				</scroll-view>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: '项目',
-				tabbar: ''
-			}
+export default {
+	data() {
+		return {
+			topics: [],
+			// 因为内部的滑动机制限制，请将tabs组件和swiper组件的current用不同变量赋值
+			current: 0, // tabs组件的current值，表示当前活动的tab选项
+			swiperCurrent: 0, // swiper组件的current值，表示当前那个swiper-item是活动的
+			page: 0,
+			list: [],
+		};
+	},
+	onLoad() {
+		this.getProjectTopic();
+	},
+	methods: {
+		getProjectTopic() {
+			this.$u.api.projectTopic().then(res => {
+				console.log(res);
+				this.topics = res;
+				if (this.topics.length >= 1) {
+					let first = this.topics[0]
+					let id = first.id
+					this.getProjectList(id)
+				}
+			});
 		},
-		onLoad() {
-
+		getProjectList(id) {
+			this.$u.api.projectList({"cid": id.toString(),}, 0).then(res => {
+				this.list = this.list.concat(res.datas)
+				console.log(res.datas)
+			})
+		},
+		// tabs通知swiper切换
+		tabsChange(index) {
+			this.swiperCurrent = index;
+			this.changePage(index)
+		},
+		// swiper-item左右移动，通知tabs的滑块跟随移动
+		transition(e) {
+			let dx = e.detail.dx;
+			this.$refs.uTabs.setDx(dx);
+		},
+		// 由于swiper的内部机制问题，快速切换swiper不会触发dx的连续变化，需要在结束时重置状态
+		// swiper滑动结束，分别设置tabs和swiper的状态
+		animationfinish(e) {
+			let current = e.detail.current;
+			this.$refs.uTabs.setFinishCurrent(current);
+			this.swiperCurrent = current;
+			this.current = current;
+		},
+		// scroll-view到底部加载更多
+		onreachBottom() {
+			
+		},
+		changePage(index) {
+			this.page = 0
+			this.list = []
+			let id = this.topics[index].id
+			this.getProjectList(id)
 		}
 	}
+};
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+.swiper-item {
+	height: 100%;
+}
+	
+.swiper-box {
+	flex: 1;
+}	
+	
 </style>
