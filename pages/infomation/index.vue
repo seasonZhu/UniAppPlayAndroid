a9d8994280adbf873d9497cd4ea06e776b484baf
zhujilong <zhujilong2007@sina.com>
2021-05-10 10:00:09 +0800

    添加注释，细节优化

--- pages/infomation/index.vue
+++ pages/infomation/index.vue
@@ -26,9 +26,11 @@ export default {
 		return {
 			topics: [],
 			// 因为内部的滑动机制限制，请将tabs组件和swiper组件的current用不同变量赋值
-			current: 0, // tabs组件的current值，表示当前活动的tab选项
-			swiperCurrent: 0 ,// swiper组件的current值，表示当前那个swiper-item是活动的
-			lists: [[]],
+			// tabs组件的current值，表示当前活动的tab选项
+			current: 0, 
+			// swiper组件的current值，表示当前那个swiper-item是活动的
+			swiperCurrent: 0 ,
+			lists: [],
 			pages: [],
 			listStatus: [],
 		};
@@ -39,16 +41,28 @@ export default {
 	methods: {
 		async getProjectTopic() {
 			this.topics = await this.$u.api.projectTopic()
+			// 优化考虑用高阶map函数
 			for (let i = 0; i < this.topics.length; i++) {
 				this.pages[i] = 0
-				await this.getProjectList(i)
 			}
-			this.tabsChange(1)
-			this.tabsChange(0)
+			if(this.topics.length > 0) {
+				await this.getProjectList(0)
+			}
 		},
 		async getProjectList(index, isLoadMore = false) {
+			
+			// 如果index所在的数组有值，并且下拉状态不是nomore，那么就进行列表请求
+			let some = this.lists[index]
+			let status = this.listStatus[index]
+			if (some != undefined && status == "nomore" ) {
+				return
+			}
+			
+			// 获取请求id
 			let model = this.topics[index]
 			let id = model.id
+			
+			// 获取页数
 			let page 
 			if (isLoadMore) {
 				page = this.pages[index] + 1
@@ -57,6 +71,7 @@ export default {
 				page = this.pages[index]
 			}
 			
+			// 进行请求
 			let list = await this.$u.api.projectList({ cid: id.toString() }, page).then(res => {
 				let array = [].concat(res.datas)
 				if (isLoadMore) {
@@ -66,6 +81,7 @@ export default {
 					this.lists[index] = array
 				}
 				
+				// 虽然页面的加载数据优化好了，但是这样对于仅有一页，底部的状态文字会出现异常，我个人还是偏向于抽出这部分逻辑成为一个单页面，目前还是尝试
 				if (res.pageCount == res.curPage + 1) {
 					this.listStatus.splice(index, 1, "nomore")
 				} else {
@@ -93,6 +109,7 @@ export default {
 			this.$refs.uTabs.setFinishCurrent(current)
 			this.swiperCurrent = current
 			this.current = current
+			this.getProjectList(current)
 		},
 		// scroll-view到底部加载更多
 		reachBottom() {
